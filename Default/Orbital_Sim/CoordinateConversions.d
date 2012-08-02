82598859a5c0acae96980549852edf28ff812388
Carl Mullins <carl.r.mullins@nasa.gov>
2012-08-02 16:21:38 -0500

    MAINTENANCE: Cleaning up working directory

---
+++ Default/Orbital_Sim/CoordinateConversions.d
@@ -0,0 +1,5 @@
+Orbital_Sim/CoordinateConversions.d Orbital_Sim/CoordinateConversions.o:  \
+ ../Orbital_Sim/CoordinateConversions.cpp \
+  ../Orbital_Sim/CoordinateConversions.h
+
+../Orbital_Sim/CoordinateConversions.h:
---
+++ Default/Orbital_Sim/Planet.d
@@ -0,0 +1,6 @@
+Orbital_Sim/Planet.d Orbital_Sim/Planet.o: ../Orbital_Sim/Planet.cpp \
+  ../Orbital_Sim/Planet.h ../Orbital_Sim/CoordinateConversions.h
+
+../Orbital_Sim/Planet.h:
+
+../Orbital_Sim/CoordinateConversions.h:
